db6ad490f180c12def970a3e44fdfa60d9ea555f
FuYin <2706117703@qq.com>
2025-08-05 16:36:23 +0800

    -[class]Tools     -[func]ReadNumber<T>         -条件判断改为委托字典

--- Utils/Tools.cs
+++ Utils/Tools.cs
@@ -4,41 +4,35 @@ namespace NBT_Parser.Utils;
 
 public static class Tools
 {
+    private static readonly Dictionary<Type, Func<Span<byte>, bool, object>> ReadMethods = new()
+    {
+        [typeof(short)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadInt16BigEndian(span) : BinaryPrimitives.ReadInt16LittleEndian(span),
+        [typeof(ushort)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadUInt16BigEndian(span) : BinaryPrimitives.ReadUInt16LittleEndian(span),
+        [typeof(int)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadInt32BigEndian(span) : BinaryPrimitives.ReadInt32LittleEndian(span),
+        [typeof(uint)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadUInt32BigEndian(span) : BinaryPrimitives.ReadUInt32LittleEndian(span),
+        [typeof(long)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadInt64BigEndian(span) : BinaryPrimitives.ReadInt64LittleEndian(span),
+        [typeof(ulong)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadUInt64BigEndian(span) : BinaryPrimitives.ReadUInt64LittleEndian(span),
+        [typeof(float)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadSingleBigEndian(span) : BinaryPrimitives.ReadSingleLittleEndian(span),
+        [typeof(double)] = (span, bigEndian) =>
+            bigEndian ? BinaryPrimitives.ReadDoubleBigEndian(span) : BinaryPrimitives.ReadDoubleLittleEndian(span)
+    };
+
     public static T ReadNumber<T>(Span<byte> bytes, bool isBigEndian) where T : struct
     {
-        if (typeof(T) == typeof(short))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadInt16BigEndian(bytes)
-                : BinaryPrimitives.ReadInt16LittleEndian(bytes));
-        if (typeof(T) == typeof(ushort))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadUInt16BigEndian(bytes)
-                : BinaryPrimitives.ReadUInt16LittleEndian(bytes));
-        if (typeof(T) == typeof(int))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadInt32BigEndian(bytes)
-                : BinaryPrimitives.ReadInt32LittleEndian(bytes));
-        if (typeof(T) == typeof(uint))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadUInt32BigEndian(bytes)
-                : BinaryPrimitives.ReadUInt32LittleEndian(bytes));
-        if (typeof(T) == typeof(long))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadInt64BigEndian(bytes)
-                : BinaryPrimitives.ReadInt64LittleEndian(bytes));
-        if (typeof(T) == typeof(ulong))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadUInt64BigEndian(bytes)
-                : BinaryPrimitives.ReadUInt64LittleEndian(bytes));
-        if (typeof(T) == typeof(float))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadSingleBigEndian(bytes)
-                : BinaryPrimitives.ReadSingleLittleEndian(bytes));
-        if (typeof(T) == typeof(double))
-            return (T)(object)(isBigEndian
-                ? BinaryPrimitives.ReadDoubleBigEndian(bytes)
-                : BinaryPrimitives.ReadDoubleLittleEndian(bytes));
-
-        throw new NotSupportedException($"不支持类型为 [{typeof(T)}] 的数字读取!");
+        try
+        {
+            return (T)ReadMethods[typeof(T)].Invoke(bytes, isBigEndian);
+        }
+        catch (Exception)
+        {
+            throw new NotSupportedException($"不支持类型为 [{typeof(T)}] 的数字读取!");
+        }
     }
 }
